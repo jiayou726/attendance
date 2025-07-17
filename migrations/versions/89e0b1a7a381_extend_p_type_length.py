4c4adee059fd18f29143a2fb85afe65f7ae49fa3
unknown <jiayou726@example.com>
2025-07-17 09:33:19 +0800

    請長度假

---
+++ migrations/versions/89e0b1a7a381_extend_p_type_length.py
@@ -0,0 +1,28 @@
+"""Extend p_type length
+
+Revision ID: 89e0b1a7a381
+Revises: 90d40742f620
+Create Date: 2025-07-17 01:20:25.000000
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '89e0b1a7a381'
+down_revision = '90d40742f620'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column('checkin', 'p_type',
+                    existing_type=sa.String(length=3),
+                    type_=sa.String(length=8),
+                    existing_nullable=False)
+
+
+def downgrade():
+    op.alter_column('checkin', 'p_type',
+                    existing_type=sa.String(length=8),
+                    type_=sa.String(length=3),
+                    existing_nullable=False)

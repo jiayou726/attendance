f934798d7e82fe16ae16f875e2f0c210f497d46e
unknown <jiayou726@example.com>
2025-07-23 09:28:43 +0800

    fix: merge alembic heads to resolve multiple head error

---
+++ migrations/versions/e76227564f17_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: e76227564f17
+Revises: 89e0b1a7a381, 8b92ec97ac5c
+Create Date: 2025-07-23 09:27:54.214720
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e76227564f17'
+down_revision = ('89e0b1a7a381', '8b92ec97ac5c')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
